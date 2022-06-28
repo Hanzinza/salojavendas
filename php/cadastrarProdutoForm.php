4f62cdd645b83825f5f0882f396413346fcd877b
AndreiLGomes <101840378+AndreiLGomes@users.noreply.github.com>
2022-06-28 20:34:08 -0300

    excluir produto - OK

--- php/cadastrarProdutoForm.php
+++ php/cadastrarProdutoForm.php
@@ -89,14 +89,14 @@
     <div class="col-md-8">
     <table class="table">
 		<tr>
-			<th>Descrição</th>
+			<th>Nome</th>
 			<th>Preço</th>
 			<th>Categoria</th>
 			<th>Ações</th>
 		</tr>
         <tr>
             <?php
-            $comando = "SELECT p.nome, p.preco, c.nome as nomeCategoria FROM produtos p
+            $comando = "SELECT p.*, c.nome as nomeCategoria FROM produtos p
             INNER JOIN categorias c
             ON p.categorias_idCategoria = c.idCategoria";
 
@@ -117,17 +117,31 @@
                  while($p = mysqli_fetch_assoc($resultado)){
                      array_push($produtosRetornadas, $p);
                  } // fechamento do while
-                 foreach($produtosRetornadas as $p)
+                 foreach($produtosRetornadas as $p){
                  echo "<tr>";
                  echo "<td>".$p['nome']."</td>";
                  echo "<td>".$p['preco']."</td>";
                  echo "<td>".$p['nomeCategoria']."</td>";
-                 echo "<td>".$p['nome']."</td>";
-             }
-
             ?>
+                <td>
+                <form action="excluirProduto.php" method="POST" class="formAcao">
+				<input type="hidden" name="idProduto" value="<?=$p['idProduto']?>">
+				<button type="submit" class="botaoAcao">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+					<path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+					</svg>
+				</button>				
+			</form>			
+			</td>
+		</tr>
 
-
+            <?php
+                    }//fechamento do foreach
+                }// fechamento do else
+            ?>
+            </table>
+            </div>
+            </div>
            
         </tr>
 </table>
