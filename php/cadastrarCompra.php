cb1e39fe3a55dc51fbe52747dfc0dc6e697d167a
Pedro Braunn <85997077+pedrohbraunn@users.noreply.github.com>
2022-06-29 21:58:29 -0300

    compra - em andamento

---
+++ php/cadastrarCompra.php
@@ -0,0 +1,10 @@
+<?php
+require_once("conexaoBanco.php");
+
+$categoria = $_POST['categoria'];
+$produto = $_POST['produto'];
+$preco = $_POST['preco'];
+$quantidade = $_POST['quantidade'];
+$formapag = $_POST['formapag'];
+
+?>
