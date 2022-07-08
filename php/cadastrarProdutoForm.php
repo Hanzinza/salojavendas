e863cf7910912e8fdc405900b0f8b845b855ef03
AndreiLGomes <101840378+AndreiLGomes@users.noreply.github.com>
2022-07-07 21:18:49 -0300

    login nao

--- php/cadastrarProdutoForm.php
+++ php/cadastrarProdutoForm.php
@@ -1,4 +1,8 @@
+<?php
+session_start();
+if(isset($_SESSION['nivel']) && $_SESSION['nivel']=="1"){
 
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -197,3 +201,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<?php
+}else{
+	header("Location: alertaEfetuarLogin.html");
+}
+
+?>
